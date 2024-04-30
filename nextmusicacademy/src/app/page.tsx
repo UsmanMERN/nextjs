4ef9cc77ecc4c29c6bace8048abcf763bd421d27
UsmanMern <mu1363131@gmail.com>
2024-04-30 23:08:27 +0500

    hero and courses sections don

--- nextmusicacademy/src/app/page.tsx
+++ nextmusicacademy/src/app/page.tsx
@@ -1,8 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/ui/FeaturedCourses";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl text-center">am learning next js</h1>
+    <main className="min-h-screen bg-grid-white/[0.02] bg-black/[0.96] antialiased">
+      <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
